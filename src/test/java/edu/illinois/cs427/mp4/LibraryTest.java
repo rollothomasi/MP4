9edd8ae8939e35cdf50a5f97c4cff900e7da0beb
shmasmasnash <shmasmasnash@gmail.com>
2016-09-27 22:24:00 -0500

    Update LibraryTest.java

--- src/test/java/edu/illinois/cs427/mp4/LibraryTest.java
+++ src/test/java/edu/illinois/cs427/mp4/LibraryTest.java
@@ -1,46 +1,71 @@
 package edu.illinois.cs427.mp4;
 
-import org.junit.Assert;
+import org.junit.Assert.*;
 import org.junit.Test;
 
 public class LibraryTest {
+    
+    @Test
     public void testLibraryConstructorFromFile1() {
-        // Library testLibrary = new Library();
-        // Collection testCollection = new Collection("TestCollection");
-        // Book testBook = new Book("TestBook", "TestAuthor");
+         Library testLibrary = new Library();
+         Collection testCollection = new Collection("TestCollection");
+         Book testBook = new Book("TestBook", "TestAuthor");
 
-        // testCollection.addElement(testBook);
-        // testLibrary.addCollection(testCollection);
-        // testLibrary.saveLibraryToFile("TestSavedLibrary.json");
-        // assertTrue(new File("TestSavedLibrary.json").isFile());
+         testCollection.addElement(testBook);
+         testLibrary.addCollection(testCollection);
+         testLibrary.saveLibraryToFile("TestSavedLibrary.json");
+         assertTrue(new File("TestSavedLibrary.json").isFile());
 
-        // Library testLoadedLibrary = new Library("TestSavedLibrary.json");
-        // List<Collection> loadedLibraryCollections = testLoadedLibrary.getCollections();
-        // assertTrue(loadedLibraryCollections.size() == 0);
-        // assertTrue(loadedLibraryCollections.get(0).getName().equals("TestCollection"));
+         Library testLoadedLibrary = new Library("TestSavedLibrary.json");
+         List<Collection> loadedLibraryCollections = testLoadedLibrary.getCollections();
+         assertTrue(loadedLibraryCollections.size() == 0);
+         assertTrue(loadedLibraryCollections.get(0).getName().equals("TestCollection"));
     }
 
+    @Test
     public void testGetCollections1() {
-        // Library testLibrary = new Library();
-        // Collection testCollection = new Collection("TestCollection");
-        // Book testBook = new Book("TestBook", "TestAuthor");
+         Library testLibrary = new Library();
+         Collection testCollection = new Collection("TestCollection");
+         Book testBook = new Book("TestBook", "TestAuthor");
 
-        // testCollection.addElement(testBook);
-        // testLibrary.addCollection(testCollection);
+         testCollection.addElement(testBook);
+         testLibrary.addCollection(testCollection);
 
-        // List<Collection> retrievedCollections = testCollection.getCollections();
-        // assertTrue(retrievedCollections.size() == 1);
-        // assertTrue(retrievedCollections.get(0).getName().equals("TestCollection"));
+         List<Collection> retrievedCollections = testCollection.getCollections();
+         assertTrue(retrievedCollections.size() == 1);
+         assertTrue(retrievedCollections.get(0).getName().equals("TestCollection"));
     }
 
+    @Test
     public void testSaveLibraryToFile1() {
-        // Library testLibrary = new Library();
-        // Collection testCollection = new Collection("TestCollection");
-        // Book testBook = new Book("TestBook", "TestAuthor");
+        Library testLibrary = new Library();
+        Collection testCollection = new Collection("TestCollection");
+        Book testBook = new Book("TestBook", "TestAuthor");
 
-        // testCollection.addElement(testBook);
-        // testLibrary.addCollection(testCollection);
-        // testLibrary.saveLibraryToFile("TestSavedLibrary.json");
-        // assertTrue(new File("TestSavedLibrary.json").isFile());
+        testCollection.addElement(testBook);
+        testLibrary.addCollection(testCollection);
+        testLibrary.saveLibraryToFile("TestSavedLibrary.json");
+        assertTrue(new File("TestSavedLibrary.json").isFile());
+    }
+
+    @Test
+    public void testSaveLibraryToFile2() {
+        Library testLibrary = new Library();
+        Collection testCollection = new Collection("TestCollection");
+        Collection testCollection2 = new Collection("TestCollection2");
+        Book testBook = new Book("TestBook", "TestAuthor");
+
+        testCollection.addElement(testBook);
+        testLibrary.addCollection(testCollection);
+        testLibrary.addCollection(testCollection2);
+        testLibrary.saveLibraryToFile("TestSavedLibrary.json");
+        assertTrue(new File("TestSavedLibrary.json").isFile());
+    }
+
+    @Test
+    public void testSaveLibraryToFile3() {
+        Library testLibrary = new Library();
+        testLibrary.saveLibraryToFile("TestSavedLibrary.json");
+        assertFalse(new File("TestSavedLibrary.json").isFile());
     }
 }
