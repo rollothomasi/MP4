2beaf5fb9f56d1cd3a694fc7e0a1f4aa2d0ef169
shmasmasnash <shmasmasnash@gmail.com>
2016-09-27 20:03:04 -0500

    Update BookTest.java

--- src/test/java/edu/illinois/cs427/mp4/BookTest.java
+++ src/test/java/edu/illinois/cs427/mp4/BookTest.java
@@ -6,21 +6,25 @@ public class BookTest {
 
     Book b = new Book("Story","Joe");
 
+    @Test
     public void testBookConstructor1() {
         //TODO implement this
         assertEquals("Joe", b.getAuthor());
     }
 
+    @Test
     public void testBookConstructor() {
         //TODO implement this
         assertEquals("Story", b.getTitle());
     }
 
+    @Test
     public void testGetStringRepresentation1() {
         //TODO implement this
         assertEquals("{\"title\":\"Story\",\"author\":\"Joe\",\"className\":\"Book\"}", b.getStringRepresentation());
     }
 
+    @Test
     public void testGetContainingCollections1() {
         //TODO implement this
         assertEquals(b.getContainingCollections.size(),0);
